a856b43b6c41a012e629737ffbbc8c4d28ea1111
Plamen Draganov <plamine95@gmail.com>
2025-09-28 19:25:10 +0300

    Add About Page

--- src/index.js
+++ src/index.js
@@ -18,8 +18,12 @@ app.use(express.static('src/public'));
 
 // Routes
 app.get('/', (req, res) => {
-   res.render('home', {layout: false}); 
+   res.render('home'); 
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
 // Start Server
 app.listen(3000, () => {

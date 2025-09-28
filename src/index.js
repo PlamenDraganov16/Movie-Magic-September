3e097c8dd78554c085b01d147d249c18c0d68bfc
Plamen Draganov <plamine95@gmail.com>
2025-09-28 19:02:07 +0300

    add handlebars home

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@ import handlebars from 'express-handlebars';
 
 const app = express();
 
+// Setup Handlebars
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 
 app.set('view engine', 'hbs');
+app.set('views', 'src/views');
 
+
+// Middleware
 app.set(express.static('public'));
 
+// Routes
+app.get('/', (req, res) => {
+   res.render('home', {layout: false}); 
+});
+
+// Start Server
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });

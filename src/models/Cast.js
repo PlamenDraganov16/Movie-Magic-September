4ee8d35231adbf1e2811edb67b0397157c74080d
Plamen Draganov <plamine95@gmail.com>
2025-10-04 21:51:05 +0300

    Update Cast.js

---
+++ src/models/Cast.js
@@ -0,0 +1,27 @@
+import { Schema, model, Types } from 'mongoose';
+
+const castSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Cast name is required'],
+    },
+    age: {
+        type: Number,
+        required: [true, 'Cast age is required'],
+        max: 120,
+        min: 0,
+    },
+    born: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+
+});
+
+const Cast = model('Cast', castSchema);
+
+export default Cast

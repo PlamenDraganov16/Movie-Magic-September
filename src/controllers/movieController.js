f8efd911e05a7bb3761ba54099756eb22b611fee
Plamen Draganov <plamine95@gmail.com>
2025-10-17 18:23:57 +0300

    validation and error handleling in controllers

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -2,34 +2,60 @@ import { Router } from "express";
 import movieService from "../services/movieService.js";
 import castService from "../services/castService.js";
 import { isAuth } from "../middlewares/authMiddleware.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
+import { isMovieCreator } from "../middlewares/movieMiddleware.js";
 
 const movieController = Router();
 
 movieController.get('/create', isAuth, (req, res) => {
-    res.render('movies/create');
-})
+    if (req.isAuthenticated) {
+        console.log(req.user.email);
+    }
+
+    res.render('movies/create', { categories: getMovieCategoryViewData() });
+});
 
 movieController.post('/create', isAuth, async (req, res) => {
     const movieData = req.body;
-    const creatorId = req.user.id;
+    const userId = req.user.id;
 
-    await movieService.create(movieData, creatorId);
+    try {
+        await movieService.create(movieData, userId);
 
-    res.redirect('/');
-
+        res.redirect('/');
+    } catch (err) {
+        res.status(400).render('movies/create', {
+            error: getErrorMessage(err),
+            movie: movieData,
+            categories: getMovieCategoryViewData(movieData.category),
+        });
+    }
 });
 
 movieController.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
-    const movie = await movieService.getOneDetailed(movieId);
 
-    // const isOwner = movie.creator === req.user?.id;
-    const isOwner = movie.creator && movie.creator.equals(req.user?.id);
+    try {
+        const movie = await movieService.getOneDetailed(movieId)
 
-    const ratingViewData = '&#x2605; '.repeat(Math.trunc(movie.rating));
+        // TODO Prepare view data (temp solution)
+        const ratingViewData = '&#x2605;'.repeat(Math.trunc(movie.rating));
 
-    res.render('movies/details', { movie, rating: ratingViewData, isOwner})
+        // const isCreator = req.user?.id && movie.creator == req.user.id;
+        const isCreator = movie.creator && movie.creator.equals(req.user?.id);
 
+        res.render('movies/details', { movie, rating: ratingViewData, isCreator });
+    } catch (err) {
+        // #1 Redirect without message (not recomended)
+        // res.redirect('/404');
+
+        // #2 Render 404 page with message (url not changed)
+        // res.render('404', { error: 'Movie not found!' })
+
+        // #3 Redirect with message, url changed
+        req.tempData = { error: 'Movie not found!' };
+        res.redirect('/404');
+    }
 });
 
 movieController.get('/search', async (req, res) => {
@@ -37,15 +63,15 @@ movieController.get('/search', async (req, res) => {
 
     const movies = await movieService.getAll(filter);
 
-    res.render('search', { movies, filter })
+    res.render('search', { movies, filter, pageTitle: 'Search Movies' });
 });
 
 movieController.get('/:movieId/attach', async (req, res) => {
     const movieId = req.params.movieId;
 
     const movie = await movieService.getOne(movieId);
-    const casts = await castService.getAll({excludes: movie.casts});
-    
+    const casts = await castService.getAll({ excludes: movie.casts });
+
     res.render('casts/attach', { movie, casts });
 });
 
@@ -62,7 +88,7 @@ movieController.get('/:movieId/delete', isAuth, async (req, res) => {
     const movieId = req.params.movieId;
 
     const movie = await movieService.getOne(movieId);
-    // Check if creator
+
     if (!movie.creator?.equals(req.user.id)) {
         return res.redirect('/');
     }
@@ -72,23 +98,36 @@ movieController.get('/:movieId/delete', isAuth, async (req, res) => {
     res.redirect('/');
 });
 
-movieController.get('/:movieId/edit', isAuth, async (req, res) => {
+movieController.get('/:movieId/edit', isAuth, isMovieCreator, async (req, res) => {
     const movieId = req.params.movieId;
 
-    const movie = await movieService.getOne(movieId);
+    try {
+        const movie = await movieService.getOne(movieId);
 
-    const categoriesViewData = getMovieCategoryViewData(movie.category);
+        const categoriesViewData = getMovieCategoryViewData(movie.category);
 
-    res.render('movies/edit', { movie, categories: categoriesViewData});
+        res.render('movies/edit', { movie, categories: categoriesViewData });
+    } catch (err) {
+        res.render('404', { error: 'Movie not found' })
+    }
 });
 
-movieController.post('/:movieId/edit', async (req, res) => {
+movieController.post('/:movieId/edit', isAuth, isMovieCreator, async (req, res) => {
     const movieId = req.params.movieId;
     const movieData = req.body;
 
-    await movieService.edit(movieId, movieData);
+    try {
+        await movieService.edit(movieId, movieData);
 
-    res.redirect(`/movies/${movieId}/details`)
+        res.redirect(`/movies/${movieId}/details`);
+    } catch (err) {
+        res.status(400).render('movies/edit', {
+            error: getErrorMessage(err),
+            movie: movieData,
+            categories: getMovieCategoryViewData(movieData.category)
+        });
+    }
+
 });
 
 function getMovieCategoryViewData(selectedCategory) {

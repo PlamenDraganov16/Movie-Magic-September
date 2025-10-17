98840521557f97a6f68711afec4f37605c149d22
Plamen Draganov <plamine95@gmail.com>
2025-10-17 17:57:35 +0300

    Validation And Error handleling for models

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,30 +1,31 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, Types, model } from "mongoose";
 
 const castSchema = new Schema({
     name: {
         type: String,
-        required: [true, 'Cast name is required'],
+        required: [true, 'Cast name is required!'],
+        minLength: [5, 'Name is too short!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Name has some invalid characters!'],
     },
     age: {
         type: Number,
-        required: [true, 'Cast age is required'],
-        max: 120,
-        min: 0,
+        required: [true, 'Cast age is requried!'],
+        max: [120, 'Age cannot be more than 120!'],
+        min: [1, 'Age cannot be less than 1'],
     },
     born: {
         type: String,
-        required: true,
+        required: [true, 'Born is required'],
+        minLength: [10, 'Born should be at least 10 characters long'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Name has some invalid characters!'],
     },
     imageUrl: {
         type: String,
-        required: true,
-    },
-    movies: [{
-        type: Types.ObjectId,
-        ref: 'Movie',
-    }]
+        required: [true, 'Image url is required'],
+        match: [/^https?:\/\//, 'Image Url is invalid!'],
+    }
 });
 
 const Cast = model('Cast', castSchema);
 
-export default Cast
+export default Cast;
--- src/models/User.js
+++ src/models/User.js
@@ -1,24 +1,53 @@
-import { Schema, model, Types} from 'mongoose';
-
+import { Schema, model } from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'User email is requried'],
+        unique: [true, 'Email should be unique'],
+        match: [/[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/],
+        minLength: [10, 'Email should be at least 10 characters long!'],
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
+        match: [/^[a-zA-Z0-9]+$/],
+        minLength: [6, 'Password should be at least 6 characters'], 
+    },
+});
+
+// Validate passwords
+userSchema.virtual('rePassword')
+    .get(function() {
+        return this._rePassword;
+    })
+    .set(function(value) {
+        this._rePassword = value;
+    });
+
+userSchema.pre('validate', function() {
+    if (this.isNew && this.password !== this.rePassword) {
+        this.invalidate('rePassword', 'Password missmatch!');
     }
 });
 
-// Hash password
+// Validate unique email on user creation
+// userSchema.pre('validate', async function() {
+//     if (this.isNew) {
+//         // const userExists = await model('User').exists({email: this.email});
+//         const userExists = await this.constructor.exists({email: this.email});
+//         if (userExists) {
+//             throw new Error('User already exists!');
+//         }
+//     }
+// });
+
 userSchema.pre('save', async function () {
     // Generate salt
     // const salt = await bcrypt.genSalt(12);
-
-    this.password = await bcrypt.hash(this.password, 12);
+    
+    this.password = await bcrypt.hash(this.password, 13);
 });
 
 const User = model('User', userSchema);
